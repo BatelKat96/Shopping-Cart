ff29e9d3bb9f7b08bb3fe99828c57b221ed33385
BatelKat96 <batelkat@gmail.com>
2023-02-05 16:11:13 +0200

    Fix layout

--- frontend/src/cmps/app-header.jsx
+++ frontend/src/cmps/app-header.jsx
@@ -2,12 +2,15 @@ import { NavLink } from 'react-router-dom'
 
 export function AppHeader() {
 	return (
-		<header className="app-header">
-			<nav>
-				<NavLink to="/">Products</NavLink>
-				<NavLink to="/cart">Shopping Cart</NavLink>
-			</nav>
-			<h1>My App</h1>
+		<header className="app-header-section main-container">
+			<div className="app-header  main-container ">
+
+				<h1>My App</h1>
+				<nav>
+					<NavLink to="/">Products</NavLink>
+					<NavLink to="/cart">Shopping Cart</NavLink>
+				</nav>
+			</div>
 		</header>
 	)
 }
