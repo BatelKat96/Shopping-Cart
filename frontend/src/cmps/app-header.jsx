77a314148b431fdf70732e0af8923f05a879cebb
Drorka~ <drorpa4@gmail.com>
2023-02-05 16:42:20 +0200

    fix page header

--- frontend/src/cmps/app-header.jsx
+++ frontend/src/cmps/app-header.jsx
@@ -8,7 +8,7 @@ export function AppHeader() {
 	return (
 		<header className="app-header-section main-container">
 			<div className="app-header  main-container ">
-				<h1>My App</h1>
+				<h1>Everything and More...</h1>
 				<nav>
 					<NavLink to="/">Products</NavLink>
 					<NavLink to="/cart">Shopping Cart ({totalItems})</NavLink>
